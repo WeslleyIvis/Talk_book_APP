1b54f131c55bf6e2b0448872e4ca248404f39bad
Fernando Hideki Tomiyoshi <125675491+Fernando-Hideki@users.noreply.github.com>
2024-11-21 20:12:51 -0300

    tela do autor

---
+++ app/src/main/java/com/example/starteraplication/ui/BookDetails.kt
@@ -0,0 +1,21 @@
+package com.example.starteraplication.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.starteraplication.R
+
+class BookDetails : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_details)
+
+        val textnavigate = findViewById<TextView>(R.id.author_name_button)
+        textnavigate.setOnClickListener {
+            // Criar uma intent para navegar para outra Activity
+            val intent = Intent(this, authorScreen::class.java)
+            startActivity(intent)
+        }
+    }
+}
